910bbc96e936852c00182e4376556129366c1c12
elimut <121231529+elimut@users.noreply.github.com>
2023-05-05 12:20:46 +0200

    mutateur

--- Animal.java
+++ Animal.java
@@ -1,8 +1,8 @@
 public class Animal {
     private String aType;
-        private String aCri;
-        private int aPatte;
-        private boolean aQueue;
+    private String aCri;
+    private int aPatte;
+    private boolean aQueue;
 
     public Animal(String type, String cri, int patte, boolean queue){
         this.aType = type;
